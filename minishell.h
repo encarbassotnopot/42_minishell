f89766502eee1946c6e4a941da7036d497abed82
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-11-19 13:33:06 +0100

    primera tokenització

--- minishell.h
+++ minishell.h
@@ -6,32 +6,49 @@
 /*   By: smercado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:19:09 by smercado          #+#    #+#             */
-/*   Updated: 2024/11/19 10:25:48 by smercado         ###   ########.fr       */
+/*   Updated: 2024/11/19 13:29:35 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "readline/readline.h"
-# include "readline/history.h"
 # include <stdio.h>
+# include <stdbool.h>
 # include <stdlib.h>
 # include <string.h>
+# include "libft.h"
+# include "readline/history.h"
+# include "readline/readline.h"
 
-typedef enum s_token_type
+typedef enum e_token_type
 {
+	TYPE_UNSET,
 	WORD,
 	OPERATOR,
 	QUOTE,
-} t_token_type;
+	DQUOTE,
+}					t_token_type;
 
+typedef enum e_operator_type
+{
+	OP_UNSET,
+	LESS,
+	GREAT,
+	DLESS,
+	DGREAT,
+	PIPE,
+}					t_operator_type;
+
+/**
+ *
+ */
 typedef struct s_token
 {
-	char			*token_node;
-	t_token_type	t_type;
+	char			*char_buf;
+	t_token_type	type;
+	t_operator_type	oper;
+	bool			terminated;
 	struct s_token	*next;
-} t_token;
-
-
-# endif
+}					t_token;
+#endif
--- readline.c
+++ readline.c
@@ -6,62 +6,137 @@
 /*   By: smercado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:12:52 by smercado          #+#    #+#             */
-/*   Updated: 2024/11/19 10:49:58 by smercado         ###   ########.fr       */
+/*   Updated: 2024/11/19 13:27:07 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-enum operators {"<", ">", "|", "<<", ">>", "&"};
-enum quotes {""", "'"};
-
-int	main(int arc, char **argv)
+t_token	*append_token(t_token *token)
 {
-	char *line;
+	t_token	*my_token;
+
+	my_token = ft_calloc(1, sizeof(t_token));
+	if (token)
+		token->next = my_token;
+	/*my_token->type = 0;*/
+	/*my_token->oper = 0;*/
+	/*my_token->terminated = 0;*/
+	/*my_token->next = 0;*/
+	return (my_token);
+}
+
+void	append_q_buf(char c, t_token *cur_tok)
+{
+	if (cur_tok->type == QUOTE && c == '\'')
+		return ;
+	if (cur_tok->type == DQUOTE && c == '"')
+		return ;
+	cur_tok->char_buf[ft_strlen(cur_tok->char_buf)] = c;
+}
+
+void	expand_or_create_operator(char c, t_token **cur_tok)
+{
+	(*cur_tok)->terminated = true;
+	if ((*cur_tok)->type == OPERATOR)
+	{
+		if ((*cur_tok)->oper == LESS && c == '<')
+			(*cur_tok)->oper = DLESS;
+		else if ((*cur_tok)->oper == GREAT && c == '>')
+			(*cur_tok)->oper = DGREAT;
+	}
+	else
+	{
+		*cur_tok = append_token(*cur_tok);
+		(*cur_tok)->type = OPERATOR;
+		if (c == '<')
+			(*cur_tok)->oper = LESS;
+		else if (c == '>')
+			(*cur_tok)->oper = GREAT;
+		else if (c == '|')
+			(*cur_tok)->oper = PIPE;
+	}
+}
+
+void	append_w_buf(char c, t_token **cur_tok, size_t len)
+{
+	if ((*cur_tok)->terminated || (*cur_tok)->type != WORD)
+	{
+		*cur_tok = append_token(*cur_tok);
+		(*cur_tok)->char_buf = ft_calloc(len, sizeof(char));
+		(*cur_tok)->type = WORD;
+	}
+	(*cur_tok)->char_buf[ft_strlen((*cur_tok)->char_buf)] = c;
+}
+
+void	init_quote(char c, t_token **cur_tok, size_t len)
+{
+	*cur_tok = append_token(*cur_tok);
+	if (c == '\'')
+		(*cur_tok)->type = QUOTE;
+	else if (c == '"')
+		(*cur_tok)->type = DQUOTE;
+	(*cur_tok)->char_buf = ft_calloc(len, sizeof(char));
+}
+
+t_token	*tokenization(char *line)
+{
+	int		len;
+	t_token	*tokens;
+	t_token	*cur_tok;
+
+	tokens = append_token(NULL);
+	cur_tok = tokens;
+	len = ft_strlen(line);
+	while (*line)
+	{
+		if (!cur_tok)
+			exit(1);
+		if (cur_tok->type == QUOTE || cur_tok->type == DQUOTE)
+			append_q_buf(*line, cur_tok);
+		else if (ft_strchr("<>|", *line))
+			expand_or_create_operator(*line, &cur_tok);
+		else if (ft_strchr("\'\"", *line))
+			init_quote(*line, &cur_tok, len);
+		else if (ft_isspace(*line))
+			cur_tok->terminated = true;
+		else
+			append_w_buf(*line, &cur_tok, len);
+		line++;
+	}
+	cur_tok->terminated = true;
+	return (tokens);
+}
+
+void tok_debug(t_token *t)
+{
+	while (t)
+	{
+		printf("type: %i\n", t->type);
+		printf("chbuf: %s\n", t->char_buf);
+		printf("operator: %i\n", t->oper);
+		printf("terminated?: %i\n", t->terminated);
+		t = t->next;
+	}
+}
+
+int	main()
+{
+	char	*line;
+	t_token	*tokens;
 
 	while (1)
 	{
 		line = readline("minishel_test1: ");
-		printf("line is: %s", line);
-
-	//errors_checker
-		if (!line)
-			break ;
 		if (*line)
 		{
 			add_history(line);
-			tokenization(line);
+			tokens = tokenization(line);
+			tok_debug(tokens);
 		}
 		free(line);
 	}
-	exit (0);
+	exit(0);
 }
 
-void tokenization(char *line)
-{
-	char	buffer[99999];
-	t_token	*token;
-	t_token_type current_type;
-	int		i;
-	char	c;
-
-	i = 0;
-	token = NULL;
-	current_type = WORD;
-	while (line[i] != '\0')
-	{
-		if (line[i + 1] == '\0' && buffer[i] != '\0')
-		{
-			buffer[i] = line[i];
-			buffer[i + 1] = '\0';
-		//	add_token();
-		}
-		if (is_operator(line[i]) && (!is_quote(line[i]) && (line[i] == operators))
-				//check if tokens se pueden unir y funcionan juntos
-				act_token = ft_strjoin(act_token, (char *)line[i]);
-		if 
-	}
-
-}
 
