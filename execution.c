8ee471a90ab1c88d4c67bfb1661196aaa30a0e34
eina <109956262+encarbassotnopot@users.noreply.github.com>
2024-12-19 17:56:20 +0100

    té problemes però va fent

--- execution.c
+++ execution.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 08:51:33 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/12/19 12:24:59 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/12/19 12:53:43 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,82 @@ char	*get_exe(char *path, char *name)
 }
 
 /**
- * Runs the given command and sets up its redirections.
+ * Atempts opening a file for reading.
+ * Returns its fd or -1 on error.
+ */
+int	redir_read(char *file)
+{
+	int	fd;
+
+	if (access(file, R_OK) == -1)
+	{
+		perror("couldn't access file for reading");
+		return (-1);
+	}
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		perror("couldn't open file for reading");
+	return (fd);
+}
+/**
+ *  Tries to open or create a file for (truncated) writing.
+ * Returns its fd or -1 on error.
+ */
+int	redir_trunc(char *file)
+{
+	int	fd;
+
+	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd == -1)
+		perror("couldn't open/create file for writing");
+	return (fd);
+}
+/**
+ * Tries to open or create a file for writing, appending text to the end.
+ * Returns its fd or -1 on error.
+ */
+int	redir_append(char *file)
+{
+	int	fd;
+
+	fd = open(file, O_WRONLY | O_CREAT | O_APPEND,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd == -1)
+		perror("couldn't open/create file for writing");
+	return (fd);
+}
+
+/**
+ * Iterates through a command's redirections and sets them up.
+ * Returns -1 on error, 0 otherwise.
+ */
+int	setup_redirs(t_command *command)
+{
+	size_t	i;
+	int		fd;
+
+	i = -1;
+	while (command->redir[++i] && command->file[i])
+	{
+		if (command->redir[i] == LESS)
+			fd = redir_read(command->file[i]);
+		else if (command->redir[i] == GREAT)
+			fd = redir_trunc(command->file[i]);
+		else if (command->redir[i] == DGREAT)
+			fd = redir_append(command->file[i]);
+		if (fd == -1)
+			return (-1);
+		if (command->redir[i] == LESS)
+			command->fds[P_READ] = fd;
+		else
+			command->fds[P_WRITE] = fd;
+	}
+	return (0);
+}
+
+/**
+ * Runs the given command.
  * This function should only be called by the child resulting from a fork.
  *
  * Edge case: a command might not even contain a command,
@@ -50,6 +125,8 @@ void	run_command(t_command *command, char **envp)
 {
 	char	*fp;
 
+	if (setup_redirs(command) == -1)
+		exit(EXIT_FAILURE);
 	if (dup2(command->fds[P_READ], STDIN_FILENO) == -1)
 		pexit("dup2 stdin");
 	if (dup2(command->fds[P_WRITE], STDOUT_FILENO) == -1)
