c6f93e2ca2d383d34068f382b2e0b22ff31bf8ef
Silvia Mercado Nicolás <smercado@student.42barcelona.com>
2025-01-08 18:05:29 +0100

    exit fixed

--- src/builtins/env_exit.c
+++ src/builtins/env_exit.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   env_exit.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecoma-ba <ecoma-ba@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smercado <smercado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:37:26 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2025/01/08 10:03:59 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2025/01/08 18:04:56 by smercado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
+
+static int	is_numeric(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 /**
  * Prints the whole environment.
@@ -33,6 +49,25 @@ int	run_env(t_shell *sh)
  */
 int	run_exit(t_shell *sh)
 {
-	cleanup(sh, NULL, 0);
+	int	arg_count;
+
+	arg_count = count_args(sh->command);
+	if (arg_count > 2)
+	{
+		printf("minishell: exit: too many arguments\n");
+		cleanup(sh, NULL, EXIT_FAILURE);
+	}
+	if (arg_count == 1)
+	{
+		cleanup(sh, NULL, 0);
+		return (EXIT_SUCCESS);
+	}
+	if (!is_numeric(sh->command->arguments[1]))
+	{
+		printf("minishell: exit: %s: numeric argument required\n",
+			sh->command->arguments[1]);
+		cleanup(sh, NULL, EXIT_FAILURE);
+	}
+	cleanup(sh, NULL, ft_atoi(sh->command->arguments[1]));
 	return (0);
 }
